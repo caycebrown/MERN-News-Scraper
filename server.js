acbcedda15e86e34070372be7ce43d083202731a
cayce brown <caycebrown98@gmail.com>
2019-07-22 10:24:03 -0500

    1

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname = '/client/public/index.html'));
+    });
   }
 
 
