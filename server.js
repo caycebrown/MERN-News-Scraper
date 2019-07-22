52f57388a111e440c5e0a73090928eab402f7f23
cayce brown <caycebrown98@gmail.com>
2019-07-22 12:18:43 -0500

    update

--- server.js
+++ server.js
@@ -19,11 +19,7 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.resolve(__dirname, "build")));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-    })
+    app.use(express.static("client/build"));
   };
 
 
