d4db18d1562ee4eda474e3341999ff541b78e0b5
cayce brown <caycebrown98@gmail.com>
2019-07-22 10:28:19 -0500

    test

--- server.js
+++ server.js
@@ -21,14 +21,11 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname = '/client/public/index.html'));
+        res.sendFile(path.join(__dirname + '/client/public/index.html'));
     });
   }
 
 
-app.get('/', function(req, res){
-    res.send(__dirname + './client/build/index.html')
-});
 
 
 app.listen(PORT, function(err) {
