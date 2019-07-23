bff0f77e9932722d73a52ad63410aefe1d643cfe
cayce brown <caycebrown98@gmail.com>
2019-07-22 22:56:04 -0500

    updated static file path

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'build', 'index.html')));
+    app.use(express.static(path.join(__dirname, 'client/build', 'index.html')));
 
     app.get('/*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
       });
   }
 
